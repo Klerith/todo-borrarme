4e0ea06dd2273b3aae7526c7546f7308beada4ce
Fernando Herrera <fernando.herrera85@gmail.com>
2023-09-02 09:56:06 -0400

    kyes and server http2

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -22,9 +22,9 @@ export class Server {
   constructor(
     private readonly options: ServerOptions
   ) {
-    const { 
-      port, 
-      staticFolder = 'public', 
+    const {
+      port,
+      staticFolder = 'public',
       certificateDir = './keys',
       ssl = false,
     } = options;
@@ -34,18 +34,29 @@ export class Server {
     this.certificateDir = certificateDir;
     this.ssl = ssl;
   }
-  
-  
+
+
   async start() {
-    
+
 
 
     //* Middlewares
     this.app.use( express.json() );
     this.app.use( express.urlencoded( { extended: true } ) ); //  x-www-form-urlencoded extended 
 
+    //https://expressjs.com/en/advanced/best-practice-performance.html
+    // const compression = require('compression')
+    // app.use(compression())
+    // Set up rate limiter: maximum of twenty requests per minute
+    // const RateLimit = require( "express-rate-limit" );
+    // const limiter = RateLimit( {
+    //   windowMs: 1 * 60 * 1000, // 1 minute
+    //   max: 20,
+    // } );
+    // // Apply rate limiter to all requests
+
     //* Static Files
-    this.app.use(express.static( this.staticFolder ));
+    this.app.use( express.static( this.staticFolder ) );
 
     //* Routes
     this.app.use( this.options.routes );
@@ -58,29 +69,29 @@ export class Server {
     //     ]
     //   })
     // });
-    
+
 
     this.listen();
   }
 
   private listen() {
 
-    if ( !this.ssl )  {
+    if ( !this.ssl ) {
       this.app.listen( this.port, () => {
-        console.log(`Server running on port ${this.port}`);
-      })
+        console.log( `Server running on port ${ this.port }` );
+      } );
       return;
     }
 
     spdy.createServer(
       {
-        key: fs.readFileSync(`${this.certificateDir}/server.key`),
-        cert: fs.readFileSync(`${this.certificateDir}/server.crt`),
+        key: fs.readFileSync( `${ this.certificateDir }/server.key` ),
+        cert: fs.readFileSync( `${ this.certificateDir }/server.crt` ),
       },
       this.app
     ).listen( this.port, () => {
-      console.log(`HTTP2 SSL Enabled Server running on port ${this.port}`);
-    })
+      console.log( `HTTP2 SSL Enabled Server running on port ${ this.port }` );
+    } );
 
   }
 
